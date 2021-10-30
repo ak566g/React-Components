10c971faa30b2820cb09603669b5cf7119800bcf
ak566g <anki01.g@gmail.com>
2021-10-30 16:24:06 +0530

    blog-app complete

--- blog-app/blog-app-frontend/src/components/header/Header.jsx
+++ blog-app/blog-app-frontend/src/components/header/Header.jsx
@@ -1,11 +1,11 @@
-import "./Header.css";
+import "./header.css";
 
 export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">BLOG</span>
+        <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className="headerImg"
