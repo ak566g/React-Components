1b3c93540d85c9cf0a1c9565ed46e61cea8f969e
ak566g <anki01.g@gmail.com>
2021-11-03 20:07:25 +0530

    theme toggler compelete

--- contextapp/src/components/Navbar.jsx
+++ contextapp/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Component } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 class Navbar extends Component {
-    static contextType = ThemeContext
+    static contextType = ThemeContext 
     render(){
         const {isLightTheme, light, dark} = this.context
         const theme = isLightTheme? light: dark
